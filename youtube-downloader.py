cb2a19655a4d7a1f232a70bcd1e635fe709c473d
mounvip1525 <mounvip1525@gmail.com>
2020-12-14 19:31:51 +0530

    second method

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -1,9 +1,16 @@
 from pytube import YouTube
 
-yt=YouTube("https://www.youtube.com/watch?v=TH3d5cyeer0")
+# yt=YouTube("https://www.youtube.com/watch?v=TH3d5cyeer0")
 
-# print(yt.streams.all())
-# print(yt.streams.get_by_itag(22))
+# # print(yt.streams.all())
+# # print(yt.streams.get_by_itag(22))
 
-dw=yt.streams.get_by_itag(22)
-dw.download("D:/")
+# dw=yt.streams.get_by_itag(22)
+# dw.download("D:/")
+
+video_links=['https://www.youtube.com/watch?v=TH3d5cyeer0','https://www.youtube.com/watch?v=wtQ_tBSrgVM']
+for i in video_links:
+    yt=YouTube(i)
+    dw=yt.streams.get_by_itag(22)
+    dw.download("D:/")
+    print("Video successfully downloaded")
