f4d6c92bcdb88fbc811119606e41cf67ec409b7f
mounvip1525 <mounvip1525@gmail.com>
2020-12-14 19:50:16 +0530

    third method

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -8,9 +8,14 @@ from pytube import YouTube
 # dw=yt.streams.get_by_itag(22)
 # dw.download("D:/")
 
-video_links=['https://www.youtube.com/watch?v=TH3d5cyeer0','https://www.youtube.com/watch?v=wtQ_tBSrgVM']
+# video_links=['https://www.youtube.com/watch?v=TH3d5cyeer0','https://www.youtube.com/watch?v=wtQ_tBSrgVM']
+video_links=open("videolinks.txt","r")
 for i in video_links:
     yt=YouTube(i)
-    dw=yt.streams.get_by_itag(22)
-    dw.download("D:/")
-    print("Video successfully downloaded")
+    try:
+        dw=yt.streams.get_by_itag(22)
+        #dw=yt.streams.first()
+        dw.download("D:/")
+        print("Video successfully downloaded")
+    except:
+        print("Download failed ",i)
